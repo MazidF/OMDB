f2ec25bb28972d4af15912bd92a74c64331391c5
m.m <m3farahani1382@gmail.com>
2022-07-27 15:14:18 +0430

    setup mvvm architecture and hilt injection

---
+++ app/src/main/java/com/example/omdb/di/AppModule.kt
@@ -0,0 +1,13 @@
+package com.example.omdb.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class AppModule  {}
--- app/src/main/java/com/example/omdb/ui/MainActivity.kt
+++ app/src/main/java/com/example/omdb/ui/MainActivity.kt
@@ -1,8 +1,11 @@
-package com.example.omdb
+package com.example.omdb.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.omdb.R
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
